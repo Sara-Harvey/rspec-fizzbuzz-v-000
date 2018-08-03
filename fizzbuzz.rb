a655c95b88f5abcb8a86304fc60e96ffa15965c1
Sara-Harvey <saraharveynyc@gmail.com>
2018-08-03 21:16:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,16 @@
 def fizzbuzz(number)
 
 if number % 3 == 0 and number % 5 == 0
-puts "FizzBuzz"
+return "FizzBuzz"
 
 elsif number % 3 == 0
-puts "Fizz"
+return "Fizz"
 
 elsif number % 5 == 0
-puts "Buzz"
+return "Buzz"
 
 else
-puts nil
+return nil
 
 end
 end
